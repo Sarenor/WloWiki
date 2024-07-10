1e502bb866dac99feabf2aeb6a5de3857b41c2f2
flurrih <tom.gabryelewicz@gmail.com>
2024-07-10 21:16:49 +0200

    add be url

--- WloWiki/src/lib/data/api.ts
+++ WloWiki/src/lib/data/api.ts
@@ -1,7 +1,7 @@
 import type { State } from '@vincjo/datatables/remote';
 
-export const reload = async (state: State) => {
-	const response = await fetch(`http://localhost:8080/api/items?${getParams(state)}`);
+export const load = async (state: State) => {
+	const response = await fetch(`https://web.astagan.pl/api/items?${getParams(state)}`);
 	return response.json();
 };
 
