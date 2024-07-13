fa2dc579f505ed345ace310887b73b5df781d533
flurrih <tom.gabryelewicz@gmail.com>
2024-07-13 19:58:41 +0200

    fix api with items

--- WloWiki/src/routes/equipment/+page.ts
+++ WloWiki/src/routes/equipment/+page.ts
@@ -4,6 +4,6 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	return {
-        equipments: await reload({pageNumber: 0} as State, "equipment", fetch)
+        equipments: await reload({pageNumber: 0} as State, "items", fetch)
 	};
 };
